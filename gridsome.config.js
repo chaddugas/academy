ce7d3f4acd610bb421410dd4e146754976af7508
Chad Dugas <chad.dug@gmail.com>
2019-10-10 11:33:40 -0600

    Initial Commit

---
+++ gridsome.config.js
@@ -0,0 +1,44 @@
+module.exports = {
+  siteName: 'Chad Dugas Portfolio',
+  transformers: {
+    remark: {
+      externalLinksTarget: '_blank',
+      externalLinksRel: ['nofollow', 'noopener', 'noreferrer'],
+      anchorClassName: 'icon icon-link',
+      plugins: [
+        // ...global plugins
+      ]
+    }
+  },
+
+  plugins: [
+    {
+      use: '@gridsome/source-filesystem',
+      options: {
+        path: 'static/data/main-nav/**/*.md',
+        typeName: 'MainNav'
+      }
+    },
+    {
+      use: '@gridsome/source-filesystem',
+      options: {
+        path: 'static/data/main-banner/**/*.md',
+        typeName: 'MainBanner'
+      }
+    },
+    {
+      use: `gridsome-plugin-netlify-cms`,
+      options: {
+        publicPath: `/admin`
+      }
+    },
+	],
+	
+  chainWebpack: config => {
+    config.module
+      .rule('pug')
+      .test(/\.pug$/)
+      .use('pug-plain-loader')
+        .loader('pug-plain-loader')
+  }
+}
