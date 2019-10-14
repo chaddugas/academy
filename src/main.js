72c11c3507a98f7e1d9bd554c099c8a5e9bd409f
Chad Dugas <chad.dug@gmail.com>
2019-10-13 23:36:46 -0600

    updates

--- src/main.js
+++ src/main.js
@@ -2,11 +2,12 @@
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 import DefaultLayout from '~/layouts/Default.vue'
 import VueMarkdown from 'vue-markdown'
-
+import smoothscroll from 'smoothscroll-polyfill'
 import '~/assets/scss/1-destyle.css'
 
+smoothscroll.polyfill();
+
 export default function (Vue, { router, head, isClient }) {
-	// Set default layout as a global component
 	Vue.component('Layout', DefaultLayout)
 	Vue.component('Markdown', VueMarkdown)
 }
