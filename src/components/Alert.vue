d2158184b09ee621b4a7f41cf5f4be0815ed0edc
Chad Dugas <chad.dug@gmail.com>
2020-03-16 20:30:48 -0600

    updates

--- src/components/Alert.vue
+++ src/components/Alert.vue
@@ -1,24 +1,29 @@
 <template lang="pug">
 transition(name="fade")
 	.alert(
-		v-if="alert.active && active",
-		:class="{'is-expanded': expanded}")
-		.alert-content
+		v-if="alert.active && visible",
+		:class="{'is-expanded': expanded, 'is-active': active}")
+		.alert-content(v-if="active === true")
 			.alert-main
 				h6.alert-title
 					i(v-if="alert.icon", :class="alert.icon")
 					span {{ alert.title }}
 				p.alert-subtitle(v-if="!expanded && alert.subtitle")
 					span {{ alert.subtitle }}
-			.alert-copy.wysiwyg(v-if="expanded && more")
-				Markdown {{ alert.copy }}
+				.alert-copy.wysiwyg(v-if="expanded && more")
+					Markdown {{ alert.copy }}
 		.alert-actions
+			.alert-open(
+				v-if="active === false",
+				@click="active = true")
+				i(:class="alert.icon")
 			.alert-close(
+				v-if="active === true",
 				@click="active = false",
 				:style="actionHeight")
-				i.fas.fa-times
+				i.fas.fa-compress-arrows-alt
 			.alert-more(
-				v-if="more"
+				v-if="more && active === true"
 				@click="expanded = !expanded",
 				:style="actionHeight")
 				i.fas.fa-info-circle
@@ -31,9 +36,10 @@ export default {
   props: ["alert", "index"],
   data() {
     return {
-      active: false,
-			expanded: false,
-			height: '100px'
+			visible: false,
+      active: true,
+      expanded: false,
+      height: "100px"
     };
   },
   computed: {
@@ -50,10 +56,10 @@ export default {
   mounted() {
     if (process.isClient)
       setTimeout(() => {
-				this.active = true;
-				setTimeout(() => {
-					this.height = this.$el.offsetHeight
-				}, 10)
+        this.visible = true;
+        setTimeout(() => {
+          this.height = this.$el.offsetHeight;
+        }, 10);
       }, 350 + this.index * 200);
   }
 };
@@ -68,22 +74,16 @@ export default {
 .alert {
   margin-bottom: 1rem;
   display: flex;
-	width: 400px;
-	max-width: 100%;
-	max-height: 100%;
+  width: 400px;
+  max-width: 100%;
+  max-height: 100%;
   pointer-events: all;
   box-shadow: 0 0 5px rgba($black, 0.25), 0 0 20px rgba($black, 0.1);
-  transition: 0.5s ease;
   background: $white;
   position: relative;
-  &.is-expanded {
-    width: 100%;
-    .alert-subtitle {
-      span {
-        opacity: 0;
-        transition: 0.2s ease;
-      }
-    }
+  align-self: flex-start;
+  &:not(.is-active) {
+    width: auto;
   }
 }
 
@@ -99,6 +99,8 @@ export default {
   flex-direction: column;
   flex: 0 0 auto;
   position: relative;
+	flex: 1 1 100%;
+	max-height: 100%;
 }
 
 .alert-title {
@@ -130,29 +132,21 @@ export default {
   }
 }
 
-@keyframes opacity {
-  100% {
-    opacity: 1;
-  }
-}
-
 .alert-copy {
   margin: 0;
   padding: 0 0.5rem 0;
-  opacity: 0;
-  animation: opacity 0.2s 0.5s ease forwards;
   overflow-y: auto;
   -webkit-overflow-scrolling: touch;
 }
 
 .alert-actions {
   display: flex;
-  flex: 0 0 60px;
-	flex-direction: column;
-	justify-content: flex-start;
+  flex-direction: column;
+  justify-content: flex-start;
   border-left: 1px solid darken($gray, 5%);
 }
 
+.alert-open,
 .alert-close,
 .alert-more {
   padding: 0.75rem;
@@ -161,12 +155,13 @@ export default {
   justify-content: center;
   flex: 1 1 100%;
   cursor: pointer;
-  transition: 0.1s ease;
+  transition: 0.25s ease;
   max-height: 80px;
   &:first-child:not(:only-child) {
     border-bottom: 1px solid darken($gray, 5%);
   }
   i {
+		transition: transform 0.25s ease;
     font-size: 1.25rem;
     &:nth-child(2) {
       margin-left: 0.25rem;
@@ -176,5 +171,13 @@ export default {
     background: $onyx;
     color: $white;
   }
+}
+
+.is-expanded {
+	.alert-more i {
+		&:last-child {
+			transform: rotate(180deg);
+		}
+	}
 }
 </style>
