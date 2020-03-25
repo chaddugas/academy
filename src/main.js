868a28495ff8e47d0c05f1cc433550cce0ba64bc
Chad Dugas <chad.dug@gmail.com>
2020-03-25 17:05:43 -0600

    remove webanimations polyfill because who knows

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 // This is the main.js file. Import global CSS and scripts here.
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
+
 import DefaultLayout from '~/layouts/Default.vue'
 import VueMarkdown from 'vue-markdown'
 import smoothscroll from 'smoothscroll-polyfill'
-import 'web-animations-js'
 import '~/assets/scss/1-destyle.css'
 
 export default function (Vue, { router, head, isClient }) {
