989c8c63624490649ad26681f6d483fd02bbfde8
Chad Dugas <chad.dug@gmail.com>
2020-03-27 13:35:35 -0600

    status update interval

--- src/pages/Status.vue
+++ src/pages/Status.vue
@@ -2,15 +2,25 @@
 	Layout-Stripped
 		main
 			div
-				img(src='https://api.netlify.com/api/v1/badges/f20078b3-e570-4100-95ce-19b3230d5891/deploy-status')
+				iframe(:src='badge')
 </template>
 
 <script>
 import LayoutStripped from '~/layouts/Stripped'
 export default {
 	name: "Status",
+	data() {
+		return {
+			badge: 'https://api.netlify.com/api/v1/badges/f20078b3-e570-4100-95ce-19b3230d5891/deploy-status'
+		}
+	},
 	components: {
 		LayoutStripped
+	},
+	mounted() {
+		setInterval(() => {
+			this.$el.querySelector('iframe').contentWindow.location.reload(true)
+		},2500)
 	}
 };
 </script>
@@ -22,5 +32,11 @@ div {
 	align-items: center;
 	padding: 3rem;
 	min-height: 100vh;
+}
+iframe {
+	border: none;
+	width: 100%;
+	height: 100%;
+	background: none;
 }
 </style>
