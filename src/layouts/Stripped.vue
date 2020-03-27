b07536f9227b9617960ed65f36e2249232f3a80e
Chad Dugas <chad.dug@gmail.com>
2020-03-27 13:27:17 -0600

    page status

---
+++ src/layouts/Stripped.vue
@@ -0,0 +1,22 @@
+<template lang="pug">
+	#academy
+		slot
+</template>
+
+<static-query>
+</static-query>
+
+<script>
+import Alerts from "@/components/Alerts"
+import Nav from "@/components/Nav"
+export default {
+	name: "Layout",
+  components: {
+    appAlerts: Alerts,
+		appNav: Nav
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ src/pages/Status.vue
@@ -0,0 +1,26 @@
+<template lang="pug">
+	Layout-Stripped
+		main
+			div
+				img(src='https://api.netlify.com/api/v1/badges/f20078b3-e570-4100-95ce-19b3230d5891/deploy-status')
+</template>
+
+<script>
+import LayoutStripped from '~/layouts/Stripped'
+export default {
+	name: "Status",
+	components: {
+		LayoutStripped
+	}
+};
+</script>
+
+<style lang="scss" scoped>
+div {
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	padding: 3rem;
+	min-height: 100vh;
+}
+</style>
